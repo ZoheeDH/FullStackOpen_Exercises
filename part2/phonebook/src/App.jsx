4b80ce84612833db16c79010bf6ea055e95a4b30
Benjamin Barrientos <36490478+ZoheeDH@users.noreply.github.com>
2024-03-01 20:43:27 -0300

    E2.16-2.17

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,12 +4,15 @@ import personService from './services/personService'
 import FilterPersons from './components/FilterPersons'
 import PersonForm from './components/PersonForm'
 import Person from './components/Person'
+import Notification from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState(null)
+  const [messageType, setMessageType] = useState('success')
 
   useEffect(() => {
     personService
@@ -27,11 +30,22 @@ const App = () => {
           .updatePerson(repeated.id, personObject)
           .then(updatedPerson => 
             setPersons(persons.map(person => person.id !== updatedPerson.id? person : updatedPerson)))
+          .catch(error => {
+            setMessageType('error')
+            setMessage(`Information of ${repeated.name} was already deleted from server`)
+            setPersons(persons.filter(person => person.id !== repeated.id))
+            setTimeout(() => 
+            setMessage(null), 5000)
+          })
       }
     } else {
       personService
         .addPerson(personObject)
-        .then(returnedPerson => setPersons(persons.concat(returnedPerson)))
+        .then(returnedPerson => {
+            setPersons(persons.concat(returnedPerson))
+            setMessageType('success')
+            setMessage(`Added ${returnedPerson.name}`)
+          })
     }
     setNewName('')
     setNewNumber('')
@@ -65,6 +79,7 @@ const App = () => {
       <h2>Phonebook</h2>
       <FilterPersons filter={filter} handleFilter={handleFilter}/>
       <h2>Add a new</h2>
+      <Notification msg={message} type={messageType}/>
       <PersonForm 
         addPerson={addPerson} 
         newName={newName} 
