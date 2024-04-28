44046f97258926016d0955759ecc0b11f0e279ee
Benjamin Barrientos <36490478+ZoheeDH@users.noreply.github.com>
2024-04-28 19:56:49 -0400

    E6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,24 +2,29 @@ import { useDispatch, useSelector } from "react-redux"
 import { voteAnecdote } from "../reducers/anecdoteReducer"
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.anecdotes)
+  const filter = useSelector(state => state.filter)
   const dispatch = useDispatch()
 
   return (
     <div>
       {anecdotes
         .sort((a, b) => b.votes - a.votes)
-        .map((anecdote) =>
-          <div key={anecdote.id}>
-            <div>
-              {anecdote.content}
-            </div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => dispatch(voteAnecdote(anecdote.id))}>vote</button>
-            </div>
-          </div>
-        )
+        .map((anecdote) => {
+          if (anecdote.content.toLowerCase().includes(filter.toLowerCase())) {
+            return (
+              <div key={anecdote.id}>
+                <div>
+                  {anecdote.content}
+                </div>
+                <div>
+                  has {anecdote.votes}
+                  <button onClick={() => dispatch(voteAnecdote(anecdote.id))}>vote</button>
+                </div>
+              </div>
+            )
+          }
+        })
       }
     </div>
   )
